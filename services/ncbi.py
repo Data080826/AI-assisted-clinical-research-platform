94d42f1f157bcb3c7c4ad798fc7d15cf6d835324
Data080826 <dgusa2009@gmail.com>
2026-06-05 20:08:18 -0700

    ncbi.py

--- services/ncbi.py
+++ services/ncbi.py
@@ -1,6 +1,11 @@
 from Bio import Entrez
+import streamlit as st
 
-def search_pubmed(query, max_results=20):
+Entrez.email = st.secrets["NCBI_EMAIL"]
+Entrez.api_key = st.secrets["NCBI_API_KEY"]
+
+
+def search_pubmed(query, max_results=10):
 
     handle = Entrez.esearch(
         db="pubmed",
@@ -9,5 +14,35 @@ def search_pubmed(query, max_results=20):
     )
 
     record = Entrez.read(handle)
+    handle.close()
 
     return record["IdList"]
+
+
+def fetch_pubmed_details(pmids):
+
+    if not pmids:
+        return []
+
+    handle = Entrez.esummary(
+        db="pubmed",
+        id=",".join(pmids)
+    )
+
+    records = Entrez.read(handle)
+    handle.close()
+
+    papers = []
+
+    for item in records:
+
+        papers.append(
+            {
+                "Title": item.get("Title", ""),
+                "Journal": item.get("FullJournalName", ""),
+                "Date": item.get("PubDate", ""),
+                "PMID": item.get("Id", "")
+            }
+        )
+
+    return papers
