86d7019dd748c5f059a84f69940c87b17ab2193d
Data080826 <dgusa2009@gmail.com>
2026-06-06 12:12:33 -0700

    study_design.py

--- modules/study_design.py
+++ modules/study_design.py
@@ -19,13 +19,23 @@ def generate_study_design(
     """
 
     prompt = f"""
-You are a clinical research expert.
+You are a clinical research methodologist.
 
 Using the following research question:
 
 {research_question}
 
-Generate:
+First determine the most appropriate study design from:
+
+- Retrospective Cohort Study
+- Prospective Cohort Study
+- Case-Control Study
+- Cross-Sectional Study
+- Randomized Clinical Trial
+
+Explain why the selected design is best.
+
+Then generate:
 
 1. Recommended Study Design
 
@@ -46,6 +56,10 @@ Generate:
 9. Statistical Analysis Plan
 
 10. Potential Limitations
+
+11. Database Requirements
+
+12. Suggested TriNetX/Epic Cosmos Cohort Definition
 """
 
     return ask_ai(prompt)
