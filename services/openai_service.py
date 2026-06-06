9d2a37749dd2e8bc72cb3ce78f034213a9da0870
Data080826 <dgusa2009@gmail.com>
2026-06-05 19:38:06 -0700

    openai_service.py

--- services/openai_service.py
+++ services/openai_service.py
@@ -1,1 +1,20 @@
+from openai import OpenAI
+import streamlit as st
 
+client = OpenAI(
+    api_key=st.secrets["OPENAI_API_KEY"]
+)
+
+def ask_ai(prompt):
+
+    response = client.chat.completions.create(
+        model="gpt-5",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
