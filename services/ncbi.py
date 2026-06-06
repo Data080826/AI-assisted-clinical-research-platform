db2bac7328330b2df905d4e3a7c4785b4125708c
Data080826 <dgusa2009@gmail.com>
2026-06-05 19:41:10 -0700

    ncbi.py

--- services/ncbi.py
+++ services/ncbi.py
@@ -1,1 +1,13 @@
+from Bio import Entrez
 
+def search_pubmed(query, max_results=20):
+
+    handle = Entrez.esearch(
+        db="pubmed",
+        term=query,
+        retmax=max_results
+    )
+
+    record = Entrez.read(handle)
+
+    return record["IdList"]
