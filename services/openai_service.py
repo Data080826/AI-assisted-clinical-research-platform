4de7b6d20b3975f0f4de6023081eb5b52db8cf45
Data080826 <dgusa2009@gmail.com>
2026-06-06 20:32:39 -0700

    openai_service.py

--- services/openai_service.py
+++ services/openai_service.py
@@ -2,23 +2,18 @@ from openai import OpenAI
 
 def ask_ai(prompt, api_key):
 
-    try:
+    client = OpenAI(
+        api_key=api_key
+    )
 
-        client = OpenAI(
-            api_key=api_key
-        )
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
 
-        response = client.chat.completions.create(
-            model="gpt-4.1-mini",
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ]
-        )
-
-        return response.choices[0].message.content
-
-    except Exception as e:
-        return f"ERROR: {str(e)}"
+    return response.choices[0].message.content
